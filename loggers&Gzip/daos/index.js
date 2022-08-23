0401150964be3dd8168a0eb827fa68bb1ed5bc57
Matias Aguilera <aguileramati50@gmail.com>
2022-08-23 11:12:01 -0300

    heroku

--- loggers&Gzip/daos/index.js
+++ loggers&Gzip/daos/index.js
@@ -6,7 +6,7 @@ const { MongoClient } = require('mongodb');
 let contenedorCarritoImportado;
 let conectionMongo;
 
-const mongo = new MongoClient(config.mongodb.mongo);
+const mongo = new MongoClient("mongodb+srv://matias:351426351@cluster0.6lyvpyc.mongodb.net/?retryWrites=true&w=majority");
 (async () => {
     await mongo.connect();
 })();
